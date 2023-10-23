9bdab800775b6dd1b6e57e09d21e414abcb33e24
alikz <ali6316k@gmail.com>
2023-10-23 12:37:30 +0330

    init player app

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ChewieController? chewieController;
+  final videoPlayerController = VideoPlayerController.networkUrl(
+    Uri.parse(
+      'https://hitaldev.ir/videos/1',
+    ),
+  );
+
+  Future<void> initPlayer() async {
+    await videoPlayerController.initialize();
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+    );
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    initPlayer();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'پخش ویدیو',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Center(
+        child: chewieController == null
+            ? CircularProgressIndicator()
+            : Chewie(
+                controller: chewieController!,
+              ),
+      ),
+    );
+  }
+}
